eacf574ef095be91d3cd9fa66e43b6abf9443364
Selfie SD <webtamil0@gmail.com>
2022-03-05 21:46:57 +0530

    Update help.py

--- help.py
+++ help.py
@@ -5,18 +5,7 @@ class Help(object):
 
     HELP_USER = "??"
 
-    HELP_TEXT ="""<b>Yardım Menüsüne Hoş Geldiniz</b>
-
-1.) Bir Video dosyası veya url gönderin.
-2.) Bir altyazı dosyası gönderin (ass veya srt)
-3.) İstediğiniz muxing türünü seçin!
-
-Dosyaya özel isim vermek için url'yi | ile ayrılmış olarak gönderin.
-<i>url|özel_adı.mp4</i>
-
-<b>Not : </b><i>Hardmux'ta yalnızca ingilizce yazı tiplerinin desteklendiğini lütfen unutmayın, diğer komut dosyaları videoda boş bloklar olarak gösterilecektir!</i> 
-
-
+    HELP_TEXT =" Ask Here @FlimRequest"
 
     HELP_BUTTONS = InlineKeyboardMarkup(
         [[
